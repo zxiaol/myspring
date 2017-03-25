6ab943429a27eb45f5e44436035ca05390438bc0
zxiaol <zhuxiaolong05@126.com>
2017-03-25 12:22:54 +0800

    Add beans

--- src/main/java/com/xiaomingyk/myspring/MainApp.java
+++ src/main/java/com/xiaomingyk/myspring/MainApp.java
@@ -1,5 +1,8 @@
 package com.xiaomingyk.myspring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by XZhu7 on 3/25/2017.
  */
@@ -7,6 +10,13 @@ public class MainApp {
 
     public static void main(String[] args){
 
-        System.out.println("hello world!");
+        System.out.println("starting ....");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+        HelloWorld hello = (HelloWorld) context.getBean("HelloWorld");
+
+        hello.getMessage();
+
     }
 }
